c3984612ad2706f9225c79f2847e7da10351e107
Erick Wellington <160539840+erickwelli@users.noreply.github.com>
2025-09-22 17:30:31 +0000

    Criando templates e parâmetros

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "PSI é a melhor matéria do mundo!"
+    return render_template('index.html')
+
+@app.route('/perfil', defaults = {'nome': 'Fulano'})
+
+@app.route('/perfil/<nome>')
+def perfil(nome):
+    return render_template('perfil.html', nome = nome)
 
 @app.route('/contato')
 def contato():
-    return 'erick.w@escolar.ifrn.edu.br'
+    return render_template('contato.html', tel='(84) 914561249')
 
 if __name__ == "__main__":
     app.run()
