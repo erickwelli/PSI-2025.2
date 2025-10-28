f8b68dfb2e48e845c182e9b6dc434f7010e19c86
Erick Wellington <160539840+erickwelli@users.noreply.github.com>
2025-10-28 21:02:16 +0000

    Enviando e recebendo parametros (incompleto)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,6 +14,16 @@ def perfil(nome):
 @app.route('/contato')
 def contato():
     return render_template('contato.html', tel='(84) 914561249')
+
+@app.route('/dados')
+def dados():
+    return render_template('dados.html')
+
+@app.route('recebedados', methods=['GET'])
+def recebedados():
+    nome = request.args['nome']
+    telefone = request.args['telefone']
+    return "{} - {}".format(nome, telefone)
 
 if __name__ == "__main__":
     app.run()
