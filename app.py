35051f21ee531cfb1f2cb3382c49de7dc38aaffb
Erick Wellington <160539840+erickwelli@users.noreply.github.com>
2025-11-03 17:32:50 +0000

    Aula 03/11/25

--- app.py
+++ app.py
@@ -19,11 +19,41 @@ def contato():
 def dados():
     return render_template('dados.html')
 
-@app.route('recebedados', methods=['GET'])
+@app.route('/recebedados', methods=['GET'])
 def recebedados():
     nome = request.args['nome']
     telefone = request.args['telefone']
     return "{} - {}".format(nome, telefone)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/verificarlogin', methods=['POST'])
+def verificarlogin():
+    login = request.form['login']
+    senha = request.form['senha']
+    if login == 'admin' and senha =='12345':
+        return 'Seja bem-vindo, Admin'
+    else:
+        return 'Você não tem permissão de acessar essa página'
+    
+@app.route('/verificaridade/<int:idade>')
+def verificaridade(idade):
+	return render_template('verificaridade.html', idade=idade)
+
+@app.route('/usuario/<nome>')
+def usuario(nome):
+	return render_template('usuario.html', nome=nome)
+
+@app.route('/produtos/<int:qtd>')
+def produtos(qtd):
+     return render_template('produtos.html', qtd=qtd)
+
+@app.route('/compras')
+def compras():
+    itens = ["arroz", "feijão", "farinha", "macarrão", "açucar", "abacaxi", "goiaba"]
+    return render_template('compras.html', itens=itens)
 
 if __name__ == "__main__":
     app.run()
